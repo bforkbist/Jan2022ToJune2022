1f34901ef87dbc0ea84960481c401efafdf1632f
bforkbist <bforkbist@gmail.com>
2022-03-08 16:50:56 -0500

    done till display the list

---
+++ employeemanagerapp/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public employees: Employee[]=[];
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(){
+      this.getEmployee();
+  }
+  public getEmployee(): void {
+    this.employeeService.getEmployee().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse)=> {
+        alert(error.message);
+      }
+    );
+  }
+}
