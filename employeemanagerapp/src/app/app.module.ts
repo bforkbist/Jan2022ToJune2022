eff401a79ba2f21eb43eaf3e3084d9a2022f171c
bforkbist <bforkbist@gmail.com>
2022-03-10 17:14:35 -0500

    ready

--- employeemanagerapp/src/app/app.module.ts
+++ employeemanagerapp/src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,HttpClientModule,FormsModule
+    BrowserModule,
+    HttpClientModule, FormsModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,31 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http'
-import { environment } from 'src/environments/environment';
 import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class EmployeeService {
-  private apiServerUrl: string = 'http://localhost:8080';
+  private apiServerUrl = 'localhost:8080';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient){}
 
-  public getEmployee(): Observable<Employee[]>{
+  public getEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
   }
 
-  public addEmployee(employee: Employee): Observable<Employee[]>{
-    return this.http.post<Employee[]>(`${this.apiServerUrl}/employee/add`, employee);
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
   }
 
-  public updateEmployee(employee: Employee): Observable<Employee[]>{
-    return this.http.put<Employee[]>(`${this.apiServerUrl}/employee/update`, employee);
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
   }
 
-  public deleteEmployee(id: number):Observable<void>{
-    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${id}`)
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
   }
-
 }
